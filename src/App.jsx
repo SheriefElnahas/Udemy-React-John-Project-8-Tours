123e95b98719761e8289181e61060831b47cce21
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-10-04 09:35:26 +0200

    movie state from TourList Component to App Component

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,21 @@ import TourList from './TourList';
 const url = 'https://course-api.com/react-tours-project';
 
 function App() {
-  // async function fetchTours(url) {
-  //   const response = await fetch(url);
-  //   const result = await response.json();
-  //   console.log(result)
-  // }
+  const [tours, setTours] = useState([]);
 
-  // useEffect(() => {
-  //   fetchTours(url);
-  // }, [])
+  async function fetchTours(url) {
+    const response = await fetch(url);
+    const result = await response.json();
+    setTours(result);
+  }
+
+  useEffect(() => {
+    fetchTours(url);
+  }, []);
+
   return (
     <div className="App">
-      <TourList />
+      <TourList tours={tours}/>
     </div>
   )
 }
--- src/TourList.jsx
+++ src/TourList.jsx
@@ -1,22 +1,7 @@
 import Tour from "./Tour";
 
-import { useState, useEffect } from "react";
-const url = "https://course-api.com/react-tours-project";
-
-export default function TourList() {
-  const [tours, setTours] = useState([]);
-
-  async function fetchTours(url) {
-    const response = await fetch(url);
-    const result = await response.json();
-    setTours(result);
-  }
-
-  useEffect(() => {
-    fetchTours(url);
-  }, []);
-
-  const toursElement = tours.map((tour) => {
+export default function TourList(props) {
+  const toursElement = props.tours.map((tour) => {
     return <Tour key={tour.id} image={tour.image} info={tour.info} name={tour.name} price={tour.price} />
   })
 
