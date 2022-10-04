ac0d43c268ec6e4e920c71de71435bcf54e4ca75
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-10-04 11:23:36 +0200

    Loading Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import {useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import TourList from './TourList';
+import TourList from "./TourList";
 
-const url = 'https://course-api.com/react-tours-project';
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
   const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchTours(url) {
+    setIsLoading(true);
     const response = await fetch(url);
     const result = await response.json();
+    setIsLoading(false);
     setTours(result);
   }
 
@@ -19,16 +22,26 @@ function App() {
 
   function removeTour(id) {
     setTours((oldTours) => {
-      return oldTours.filter(tour => tour.id !== id);
-    })
+      return oldTours.filter((tour) => tour.id !== id);
+    });
   }
+  if (tours.length === 0 && !isLoading) {
+    return (
+      <main>
+        <h1 className="title">No Trip List</h1>
+        <div className="underline"></div>
+        <button className="btn" onClick={() => fetchTours(url)}>Refresh</button>
 
-  return (
-    <div className="App">
-      <TourList tours={tours} removeTour={removeTour}/>
-      {tours.length === 0 ? <button className="btn" onClick={() => fetchTours(url)}>Refresh</button>: ""}
-    </div>
-  )
+
+      </main>
+    );
+  } else {
+    return (
+      <div className="App">
+        <TourList tours={tours} removeTour={removeTour} loading={isLoading} />
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/TourList.jsx
+++ src/TourList.jsx
@@ -1,21 +1,37 @@
 import Tour from "./Tour";
+import Loading from "./Loading";
 
 export default function TourList(props) {
-
   const toursElement = props.tours.map((tour) => {
+    return (
+      <Tour
+        key={tour.id}
+        image={tour.image}
+        info={tour.info}
+        name={tour.name}
+        price={tour.price}
+        removeTour={props.removeTour}
+        id={tour.id}
+      />
+    );
+  });
 
-    return <Tour key={tour.id} image={tour.image} info={tour.info} name={tour.name} price={tour.price} removeTour={props.removeTour} id={tour.id}/>
-  })
+  if (props.loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
 
   return (
     <main>
-        <h1 className="title">{props.tours.length > 0 ? "Our Tours" : "No Tours Left"}</h1>
-        <div className="underline"></div>
-    
+      <h1 className="title">
+        {props.tours.length > 0 ? "Our Tours" : "No Tours Left"}
+      </h1>
+      <div className="underline"></div>
 
-        {props.tours.length > 0 ? toursElement : ""}
-
-
+      {props.tours.length > 0 ? toursElement : ""}
     </main>
   );
 }
