9c40467e14bdd818432dfed3f6db7a7a1a3c487a
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-10-04 09:47:12 +0200

    Read More & Read Less Functionality

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function Tour(props) {
+  const [isReadMore, setIsReadMore] = React.useState(false);
+
   return (
-    <div className='single-tour'>
-        <img src={props.image} alt={props.name} />
-            <footer>
-                <div className="tour-info">
-                <h4>{props.name}</h4>
-                <h4 className="tour-price">{props.price}</h4>
-                </div>
-            <p>{props.info}</p>
-            <button className="delete-btn">Not Intrested</button>
-            </footer>
-       
-
+    <div className="single-tour">
+      <img src={props.image} alt={props.name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{props.name}</h4>
+          <h4 className="tour-price">{props.price}</h4>
+        </div>
+        <p>
+          {isReadMore ? props.info : `${props.info.substring(0, 150)}...`}
+          <button onClick={() => setIsReadMore(!isReadMore)}>
+            {isReadMore ? "Show Less" : "Read More"}
+          </button>
+        </p>
+        <button className="delete-btn">Not Intrested</button>
+      </footer>
     </div>
-  )
+  );
 }
-
-
